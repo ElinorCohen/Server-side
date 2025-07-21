aa691107ab34304ea2ad631934e2e77ccae63201
ElinorCohen <98434013+ElinorCohen@users.noreply.github.com>
2025-07-21 17:25:04 +0300

    Update about.js

--- routes/about.js
+++ routes/about.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 //get the about page that shows the project members
 router.get('/', (req, res,next) => {
-    const developers = [{'firstname':'Daniel','lastname':'Semerjian','id':318004504,'email':'semdaniel1@gmail.com'},
-        {'firstname':'Shay','lastname':'Solomon','id':209088442,'email':'shays433@gmail.com'},
-        {'firstname':'Elinor','lastname':'Cohen','id':322624453,'email':'elinorco24@gmail.com'}];
+    const developers = [{'firstname':'Daniel','lastname':'Semerjian','id':000000000,'email':'semdaniel1@gmail.com'},
+        {'firstname':'Shay','lastname':'Solomon','id':111111111,'email':'shays433@gmail.com'},
+        {'firstname':'Elinor','lastname':'Cohen','id':222222222,'email':'elinorco24@gmail.com'}];
     try {
         res.send(developers);
     } catch (error) {
